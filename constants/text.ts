3a710d660ef9729d401cbe6466840c48110855a6
cReAtOR0009 <epidnugoairdrop@gmail.com>
2025-07-09 05:05:06 +0100

    constants

---
+++ constants/text.ts
@@ -0,0 +1,39 @@
+export const groups = [
+  { label: "BLW-FUTA-GROUP", value: "BLW-FUTA-GROUP" },
+  { label: "BLW-AAUA-GROUP", value: "BLW-AAUA-GROUP" },
+  { label: "BLW-EKSU-GROUP", value: "BLW-EKSU-GROUP" },
+  { label: "BLW-ONDO-GROUP", value: "BLW-ONDO-GROUP" },
+];
+
+export const chapters = [
+  { label: "BLW-FUTA", value: "BLW-FUTA" },
+  { label: "BLW-ERO", value: "BLW-ERO" },
+  { label: "BLW-FECA", value: "BLW-FECA" },
+  { label: "BLW-ELIZADE", value: "BLW-ELIZADE" },
+  { label: "BLW-WESLEY", value: "BLW-WESLEY" },
+  { label: "BLW-AAUA", value: "BLW-AAUA" },
+  { label: "BLW-ABUAD", value: "BLW-ABUAD" },
+  { label: "BLW-ACHIEVERS", value: "BLW-ACHIEVERS" },
+  { label: "BLW-RUGIPO", value: "BLW-RUGIPO" },
+  { label: "BLW-EKSU", value: "BLW-EKSU" },
+  { label: "BLW-FUOYE", value: "BLW-FUOYE" },
+  { label: "BLW-IKOLE", value: "BLW-IKOLE" },
+  { label: "BLW-BOUESTI", value: "BLW-BOUESTI" },
+  { label: "BLW-FPA", value: "BLW-FPA" },
+  { label: "BLW-ADEYEMI", value: "BLW-ADEYEMI" },
+  { label: "BLW-OAUSTECH", value: "BLW-OAUSTECH" },
+];
+
+export const designations = [
+    { label: "Pastor", value: "pastor" },
+    { label: "Coordinator", value: "coordinator" },
+    { label: "PCF Leader", value: "pcf-leader" },
+    { label: "Senior Cell Leader", value: "senior-cell-leader" },
+    { label: "Cell Leader", value: "cell-leader" },
+    { label: "Department Head", value: "department-head" },
+    { label: "Bible Study Teacher", value: "bible-study-teacher" },
+    { label: "Cell Executive", value: "cell-executive" },
+    { label: "Leader", value: "leader" },
+    { label: "Member", value: "member" },
+  ];
+export const fields = ['fullName', 'email', 'phoneNumber', 'group', 'chapter', 'designation'];
